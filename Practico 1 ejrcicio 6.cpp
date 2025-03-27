423fb2bbb6aa95ae0d0382d6644c9475f053c981
J.Escalante <juanvirgilioes@gmail.com>
2025-03-27 14:35:17 -0400

    Create Practico 1 ejrcicio 6.cpp

---
+++ Practico 1 ejrcicio 6.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <string>   
+using namespace std; 
+
+int main() {
+  
+    string name = "benjamin"; 
+    int age = 26;              
+    int birth_day = 2028; 
+
+    printf("%s cumplirá %i años en el año %i", name.c_str(), age, 2028);
+
+    return 0; 
+}
