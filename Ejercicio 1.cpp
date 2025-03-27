11cf0de4ad0a1ded12696dd2d3a685ee3fd69b30
J.Escalante <juanvirgilioes@gmail.com>
2025-03-27 14:18:58 -0400

    Create Ejercicio 1.cpp
    Finalice el ejercicio 1
    :D

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+    
+    printf("   ---------\n");
+    printf(" |  /\"\"\"\"\"\\  |\n");
+    printf(" | /       \\ |\n");
+    printf(" | | 0   0 | |\n");
+    printf(" | |   v   | |\n");
+    printf(" | \\  ---  / |\n");
+    printf(" |  \\_____/  |\n");
+    printf("   ---------\n");
+
+    return 0;
+}
